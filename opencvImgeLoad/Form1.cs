a8bf417edf9b7618337590d1ea792bbf17bf0b98
diceman <diceman@naver.com>
2023-02-20 18:01:45 +0900

    init. commit.

---
+++ opencvImgeLoad/Form1.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Windows.Forms;
+using OpenCvSharp;
+
+namespace opencvImgeLoad
+{
+    public partial class Form1 : Form
+    {
+        IplImage src;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }        
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            src = new IplImage("../../../italia.jpg");            
+            //
+            OpenCV_CLASS Convert = new OpenCV_CLASS();
+            pictureBoxIpl1.ImageIpl = src;                          //원본
+            pictureBoxIpl2.ImageIpl = Convert.GrayScale(src);       //그레이스케일
+            pictureBoxIpl3.ImageIpl = Convert.InversionImage(src);  //반전
+            pictureBoxIpl4.ImageIpl = Convert.Binary(src);          //이진화
+        }
+
+        private void Form1_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            Cv.ReleaseImage(src);
+            if (src != null) src.Dispose();
+        }
+    }
+}
---
+++ opencvImgeLoad/OpenCV_CLASS.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using OpenCvSharp; //add//
+
+namespace opencvImgeLoad
+{
+    class OpenCV_CLASS: IDisposable //자동으로 메모리 관리?
+    {
+        IplImage gray;
+        IplImage inversion;
+        IplImage bin;
+
+        public IplImage GrayScale(IplImage src)
+        {
+            gray = new IplImage(src.Size, BitDepth.U8, 1);//
+            Cv.CvtColor(src, gray, ColorConversion.BgrToGray);
+            return gray;
+        }
+        public IplImage InversionImage(IplImage src)
+        {
+            inversion = new IplImage(src.Size, BitDepth.U8, 3);//
+            Cv.Not(src, inversion);
+            return inversion;
+        }
+        public IplImage Binary(IplImage src)
+        {
+            //bin = new IplImage(src.Size, BitDepth.U8, 1);//
+            bin = this.GrayScale(src);
+            Cv.CvtColor(src, bin, ColorConversion.BgrToGray);
+            Cv.Threshold(bin, bin, 150, 255, ThresholdType.Binary);
+
+            return bin;
+        }
+
+
+        public void Dispose()
+        {
+            if (gray != null) Cv.ReleaseImage(gray);
+            if (inversion != null) Cv.ReleaseImage(inversion);
+            if (bin != null) Cv.ReleaseImage(bin);
+        }
+    }
+}
